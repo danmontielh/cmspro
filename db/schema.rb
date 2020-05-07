f9d9be106344d634b9be4fc2b78c2c5b1e6fbfa5
Dan Montiel <danmontielh@gmail.com>
2020-05-06 23:21:17 -0500

    created migration for roles and cancancan have installed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_031429) do
+ActiveRecord::Schema.define(version: 2020_05_07_040927) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 2020_05_07_031429) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.boolean "superadmin_role", default: false
+    t.boolean "editor_role", default: false
+    t.boolean "user_role", default: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
