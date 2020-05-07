bbc4f9c21ea85b838fb1d67dcf83529e0fac9858
Dan Montiel <danmontielh@gmail.com>
2020-05-06 22:58:19 -0500

    config little sample controller home

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  def get_email
+    self.email
+  end
+
+  def get_name
+    self.name
+  end
+  
+  
+
 end
