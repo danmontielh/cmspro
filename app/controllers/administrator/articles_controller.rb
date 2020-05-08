ef1659d77bdd162e1c4fc1eef5a6e19b49c59e14
Dan Montiel <danmontielh@gmail.com>
2020-05-08 18:47:57 -0500

    articles administrator controller and views

--- app/controllers/administrator/articles_controller.rb
+++ app/controllers/administrator/articles_controller.rb
@@ -1,5 +1,12 @@
 class Administrator::ArticlesController < ApplicationController
+    before_action :set_administrator_articles, only: [:show, :edit, :update, :destroy]
 
+    def new
+        @article = Article.new
+    end
+    
+    
+    
     def index
         if user_signed_in?
             @articles_user = User.find(current_user.id).get_articles
@@ -7,6 +14,34 @@ class Administrator::ArticlesController < ApplicationController
             redirect_to new_user_session_path
         end
     end
-    
+
+    def create
+        @article = current_user.articles.build(articles_params)
+        if @article.save
+            redirect_to administrator_articles_path
+        else
+            render :new
+        end
+        
+    end
+
+    def create
+
+
+    end
+
+
+    private
+
+    def set_administrator_articles
+        @article = Article.find(params[:id])
+    end
+
+
+    def articles_params
+        params.require(:article).permit(:title, :content)
+    end
+
+ 
 
 end
